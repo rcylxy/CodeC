1c8f5501265914e031510f0e6ebfec3172b85f01
rcylxy <islixiaoyang2021@163.com>
2022-02-20 18:30:18 +0800

    hello

--- icoding/icoding4_Else/code1.cpp
+++ icoding/icoding4_Else/code1.cpp
@@ -1,7 +1,6 @@
 /*实现三元组表示的两个稀疏矩阵的加法。相关定义如下：*/
 #include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
 #define MAXSIZE 100 //假设非零元个数的最大值为100
 #define ElemType int
 typedef struct
@@ -17,6 +16,83 @@ typedef struct
 } TSMatrix;
 bool add_matrix(const TSMatrix *pM, const TSMatrix *pN, TSMatrix *pQ)
 {
+  int iM, iN;
+  int jM, jN;
+  int M, N, Q;
   if ((*pM).m != (*pN).m || (*pM).n != (*pN).n)
     return false;
+  pQ->m = pM->m;
+  pQ->n = pM->n;
+  for (M = 0, N = 0, Q = 0; M < pM->len && N < pN->len;)
+  {
+    iM = pM->data[M].i;
+    iN = pN->data[N].i;
+    jM = pM->data[M].j;
+    jN = pN->data[N].j;
+    if (iM > iN)
+    {
+      pQ->data[Q].e = pN->data[N].e;
+      pQ->data[Q].i = pN->data[N].i;
+      pQ->data[Q].j = pN->data[N].j;
+      N++;
+      Q++;
+    }
+    else if (iM < iN)
+    {
+      pQ->data[Q].e = pM->data[M].e;
+      pQ->data[Q].i = pM->data[M].i;
+      pQ->data[Q].j = pM->data[M].j;
+      M++;
+      Q++;
+    }
+    else if (iM == iN)
+    {
+      if (jM == jN)
+      {
+        if (pN->data[N].e + pM->data[M].e != 0)
+        {
+          pQ->data[Q].i = pM->data[M].i;
+          pQ->data[Q].j = pM->data[M].j;
+          pQ->data[Q].e = pN->data[N].e + pM->data[M].e;
+          Q++;
+        }
+        M++;
+        N++;
+      }
+      else if (jM < jN)
+      {
+        pQ->data[Q].e = pM->data[M].e;
+        pQ->data[Q].i = pM->data[M].i;
+        pQ->data[Q].j = pM->data[M].j;
+        M++;
+        Q++;
+      }
+      else if (jM > jN)
+      {
+        pQ->data[Q].e = pN->data[N].e;
+        pQ->data[Q].i = pN->data[N].i;
+        pQ->data[Q].j = pN->data[N].j;
+        N++;
+        Q++;
+      }
+    }
+  }
+  while (M < pM->len)
+  {
+    pQ->data[Q].e = pM->data[M].e;
+    pQ->data[Q].i = pM->data[M].i;
+    pQ->data[Q].j = pM->data[M].j;
+    M++;
+    Q++;
+  }
+  while (N < pN->len)
+  {
+    pQ->data[Q].e = pN->data[N].e;
+    pQ->data[Q].i = pN->data[N].i;
+    pQ->data[Q].j = pN->data[N].j;
+    N++;
+    Q++;
+  }
+  pQ->len = Q + 1;
+  return true;
 }
