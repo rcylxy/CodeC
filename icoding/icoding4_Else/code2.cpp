b1196bf944ef3856e0af724335443590558af993
rcylxy <islixiaoyang2021@163.com>
2022-02-26 11:03:05 +0800

    have finished it

--- icoding/icoding4_Else/code2.cpp
+++ icoding/icoding4_Else/code2.cpp
@@ -17,20 +17,12 @@ typedef struct
   OLink *rowhead, *colhead;
   int rows, cols, nums;
 } CrossList, *PCrossList;
-/*1）实现十字链表的初始化操作：
-int init_cross_list(PCrossList L, const ElemType *A, int m, int n);
-其中 L 指向 CrossList 结构，且各成员已被初始化为0；
-A 为 ElemType 类型  数组中第一个元素的地址，元素的个数为 m×n 个，按行优先存储
-（即A[0] 为十字链表第1行第1列的元素；
-A[1] 为第1行第2列的元素，A[n] 为第2行第1列的元素，A[n+1] 为第2行第2个元素）；
-
-m 表示十字链表的行数，n 表示十字链表的列数。
-init_cross_list 函数将 ElemType 数组中非0元素保存到十字链表中，函数返回非 0 元素的个数。
-
+/*
 2）实现十字链表的删除操作：
 int del_cross_list(PCrossList L, ElemType k);
 其中 L 指向 要处理的 CrossList 结构，k 为要删除的元素；
-del_cross_list 函数删除十字链表中所有值为 k 的结点，并返回删除结点的个数。*/
+del_cross_list 函数删除十字链表中所有值为 k 的结点，并返回删除结点的个数。
+*/
 int init_cross_list(PCrossList L, const ElemType *A, int m, int n)
 {
   OLNode *p, *q;
@@ -102,4 +94,36 @@ int init_cross_list(PCrossList L, const ElemType *A, int m, int n)
 }
 int del_cross_list(PCrossList L, ElemType k)
 {
+  int sum = 0;
+  OLNode *p, *q, *s;
+  OLNode *x, *y, *z;
+  for (int i = 0; i < L->rows; ++i)
+  {
+    p = L->rowhead[i];
+    q = p;
+    while (p)
+    {
+      if ((p->value == k) && (p == q))
+      {
+        L->rowhead[i] = L->rowhead[i]->right;
+        free(p);
+        p = L->rowhead[i];
+        sum++;
+      }
+      else if (p->value == k)
+      {
+        q->right = p->right;
+        free(p);
+        p = q->right;
+        sum++;
+      }
+      else
+      {
+        q = p;
+        p = p->right; //保留前面那一个
+      }
+    }
+  }
+  L->nums -= sum;
+  return sum;
 }
