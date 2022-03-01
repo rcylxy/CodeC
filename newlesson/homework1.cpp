abfcd96948ac2a502d6f99be9b52bb18a63bd583
rcylxy <islixiaoyang2021@163.com>
2022-03-01 20:14:33 +0800

    20;14

---
+++ newlesson/homework1.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  int i = 0, j = 0, k = 0, q = 0;
+  int prior = 1000 * i + 100 * j + 10 * k + q;
+  for (i = 1; i < 10; ++i)
+    for (j = 1; j < 10; ++j)
+      for (k = 1; k < 10; ++k)
+        for (q = 1; q < 10; ++q)
+        {
+          int prior = 1000 * i + 100 * j + 10 * k + q;
+          int parent = 1000 * q + 100 * k + 10 * j + i;
+          if (4 * prior == parent)
+          {
+            printf("%d", prior);
+            return 0;
+          }
+        }
+}
